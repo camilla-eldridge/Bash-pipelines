7a5484a2b24782f01fcbb118024c47987a502b91
Camilla Eldridge <k1116370@kingston.ac.uk>
2020-11-02 17:12:00 +0000

    Update hmm.sh

--- hmm_to_fasta/hmm.sh
+++ hmm_to_fasta/hmm.sh
@@ -14,21 +14,13 @@ if [ "$model" == "N" ];then
 	muscle -in "$query" -out "$id"_muscle.fasta
 	hmmbuild --dna "$id".hmm "$id"_muscle.fasta
 	nhmmer --dna "$id".hmm "$target" > "$id".nhmmer
-	hmm_to_fasta.py "$id".nhmmer  "$target"  "$model" > "$id"_nhmmout.fa
+	nhmmer_or_hmmsearch_to_fasta.py "$id".nhmmer  "$target"  "$model" > "$id"_nhmmout.fa
 else
 	clustalo --in "$query" -o "$id"_clustalo.fasta
 	hmmbuild --amino "$id".hmm "$id"_clustalo.fasta
 	hmmsearch "$id".hmm "$target" > "$id".hmmsearch
-	hmm_to_fasta.py "$id".hmmsearch "$target" "$model"  > "$id"_hmmsearch.fa
+	nhmmer_or_hmmsearch_to_fasta.py "$id".hmmsearch "$target" "$model"  > "$id"_hmmsearch.fa
 fi
 
 
 
-#phmmer == search with single sequence input
-#phmmer
-#(protein sequence vs protein database),
-#hmmsearch
-#(protein HMM query
-#vs protein database), or
-#hmmscan
-#(protein query vs protein HMM database)
